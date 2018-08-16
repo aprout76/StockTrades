b4b24db1608958ae208c1631cf102384f889751d
Adam Prout <ajp1465@rit.edu>
2018-08-15 22:04:58 -0400

    Initial Setup

---
+++ StockTrades/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StockTrades.MvcApplication" Language="C#" %>
---
+++ StockTrades/Models/Trade.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StockTrades.Models
+{
+    public class Trade
+    {
+
+        private String Ticker { get; set; }
+        private Decimal BuyPrice { get; set; }
+        private Decimal SellPrice { get; set; }
+        private Decimal TargetPrice { get; set; }
+        private Decimal StopPrice { get; set; }
+        
+
+
+        public Trade (String ticker, Decimal buyPrice, Decimal sellPrice, Decimal targetPrice, Decimal stopPrice)
+        {
+            Ticker = ticker;
+            BuyPrice = buyPrice;
+            SellPrice = sellPrice;
+            TargetPrice = targetPrice;
+            StopPrice = stopPrice;
+        }
+
+    }
+}
